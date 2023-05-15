c4cb4950f9e6b005565ed53d8b62516bd3dd7da1
Ke Li <li_ke@yahoo.com>
2023-05-15 10:44:22 +0400

    Add files via upload

---
+++ D2L/models/mlp/train_mlp_concise.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+from D2L.data.data import load_data_fashion_mnist
+from D2L.utils.eval import predict_fashion_mnist
+from D2L.utils.train import train
+
+
+if __name__ == '__main__':
+    batch_size = 256
+    train_iter, test_iter = load_data_fashion_mnist(batch_size)
+
+    num_inputs, num_outputs, num_hiddens = 784, 10, 256
+
+    net = nn.Sequential(nn.Flatten(),
+                        nn.Linear(num_inputs, num_hiddens),
+                        nn.ReLU(),
+                        nn.Linear(num_hiddens, num_outputs))
+
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.normal_(m.weight, std=0.01)
+
+    net.apply(init_weights)
+
+    loss = nn.CrossEntropyLoss(reduction='none')
+
+    num_epochs, lr = 5, 0.1
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    train(net, train_iter, test_iter, loss, num_epochs, optimizer)
+    predict_fashion_mnist(net, test_iter)
+
---
+++ D2L/models/mlp/train_mlp_scratch.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+from D2L.data.data import load_data_fashion_mnist
+from D2L.models.activation import relu
+from D2L.utils.eval import predict_fashion_mnist
+from D2L.utils.train import train
+
+
+def net(X):
+    X = X.reshape((-1, num_inputs))
+    H = relu(torch.matmul(X, W1)) + b1
+    return torch.matmul(H, W2) + b2
+
+
+if __name__ == '__main__':
+    batch_size = 256
+    train_iter, test_iter = load_data_fashion_mnist(batch_size)
+
+    num_inputs, num_outputs, num_hiddens = 784, 10, 256
+
+    W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True) * 0.01)
+    b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad=True))
+
+    W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.01)
+    b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
+
+    params = [W1, b1, W2, b2]
+
+    loss = nn.CrossEntropyLoss(reduction='none')
+
+    num_epochs, lr = 10, 0.1
+    optimizer = torch.optim.SGD(params, lr=lr)
+    train(net, train_iter, test_iter, loss, num_epochs, optimizer)
+    predict_fashion_mnist(net, test_iter)
+
