1798122cf93af3655dc4ce583f5eb4c0baf8840e
Ke Li <li_ke@yahoo.com>
2023-05-15 10:32:46 +0400

    Create constant.py

---
+++ D2L/constant/constant.py
@@ -0,0 +1,65 @@
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+DATA_HUB['kaggle_house_train'] = (
+        DATA_URL + 'kaggle_house_pred_train.csv', '585e9cc93e70b39160e7921475f9bcd7d31219ce')
+DATA_HUB['kaggle_house_test'] = (
+        DATA_URL + 'kaggle_house_pred_test.csv', 'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+
+DATA_HUB['time_machine'] = (
+        DATA_URL + 'timemachine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+DATA_HUB['fra-eng'] = (
+        DATA_URL + 'fra-eng.zip', '94646ad1522d915e7b0f9296181140edcf86a4f5')
+
+DATA_HUB['ptb'] = (
+        DATA_URL + 'ptb.zip', '319d85e578af0cdc590547f26231e4e31cdf1e42')
+
+DATA_HUB['glove.6b.50d'] = (
+        DATA_URL + 'glove.6B.50d.zip', '0b8703943ccdb6eb788e6f091b8946e82231bc4d')
+
+DATA_HUB['glove.6b.100d'] = (
+        DATA_URL + 'glove.6B.100d.zip', 'cd43bfb07e44e6f27cbcc7bc9ae3d80284fdaf5a')
+
+DATA_HUB['glove.42b.300d'] = (
+        DATA_URL + 'glove.42B.300d.zip', 'b5116e234e9eb9076672cfeabf5469f3eec904fa')
+
+DATA_HUB['wiki.en'] = (
+        DATA_URL + 'wiki.en.zip', 'c1816da3821ae9f43899be655002f6c723e91b88')
+
+DATA_HUB['wikitext-2'] = (
+        'https://s3.amazonaws.com/research.metamind.io/wikitext/'
+        'wikitext-2-v1.zip', '3c914d17d80b1459be871a5039ac23e752a53cbe')
+
+DATA_HUB['aclImdb'] = (
+        'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz',
+        '01ada507287d82875905620988597833ad4e0903')
+
+DATA_HUB['SNLI'] = (
+        'https://nlp.stanford.edu/projects/snli/snli_1.0.zip',
+        '9fcde07509c7e87ec61c640c1b2753d9041758e4')
+
+DATA_HUB['bert.base'] = (
+        DATA_URL + 'bert.base.torch.zip', '225d66f04cae318b841a13d32af3acc165f253ac')
+DATA_HUB['bert.small'] = (
+        DATA_URL + 'bert.small.torch.zip', 'c72329e68a732bef0452e4b96a1c341c8910f81f')
+
+DATA_HUB['hotdog'] = (
+        DATA_URL + 'hotdog.zip', 'fba480ffa8aa7e0febbb511d181409f899b9baa5')
+
+DATA_HUB['banana-detection'] = (
+        DATA_URL + 'banana-detection.zip', '5de26c8fce5ccdea9f91267273464dc968d20d72')
+
+DATA_HUB['voc2012'] = (
+        DATA_URL + 'VOCtrainval_11-May-2012.tar', '4e443f8a2eca6b1dac8a6c57641b67dd40621a49')
+
+DATA_HUB['cifar10_tiny'] = (
+        DATA_URL + 'kaggle_cifar10_tiny.zip', '2068874e4b9a9f0fb07ebe0ad2b29754449ccacd')
+
+DATA_HUB['dog_tiny'] = (
+        DATA_URL + 'kaggle_dog_tiny.zip', '0cb91d09b814ecdc07b50f31f8dcad3e81d6a86d')
+
+DATA_HUB['pokemon'] = (
+        DATA_URL + 'pokemon.zip', 'c065c0e2593b8b161a2d7873e42418bf6a21106c')
+
+
